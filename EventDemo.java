9a8d77408e975085c340fb807b463a67ba12901d
Elisia Morgan <33482645+elisiag10@users.noreply.github.com>
2022-02-05 00:15:15 -0500

    Create EventDemo.java

---
+++ EventDemo.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class EventDemo {
+
+    public static void main(String[] args) {
+        Event event = new Event();
+        event.setEventNumber(inputEventNumber());
+        event.setGuests(getGuest());
+        Main.displayBanner();
+        displayPrice(event.getEventNumber(), event.getNumberOfGuest(), event.getPrice(), event.PRICE_PER_GUEST,
+                event.CUT_OFF_VALUE);
+
+    }
+
+    public static String inputEventNumber() {
+        Scanner myObj = new Scanner(System.in); // Create a Scanner object
+        System.out.println("Enter event number: ");
+        String eventNumber = myObj.nextLine(); // Read user input
+        // getRandomNumber();
+        return eventNumber;
+    }
+
+    public static int getGuest() {
+        Scanner myObj = new Scanner(System.in); // Create a Scanner object
+        System.out.println("Enter number of guests: ");
+        int numberOfGuests = myObj.nextInt(); // Read user input
+        // getRandomNumber();
+        return numberOfGuests;
+    }
+
+    public static void displayPrice(String eventNumber, int getNumberOfGuest, int getPrice, int pricePerGuest,
+            int cutOffVal) {
+        System.out.println("This fucker here should be the event number " + eventNumber);
+        System.out.println("Number of guests: " + getNumberOfGuest);
+        System.out.println("The price per guests: " + getPrice);
+        System.out.println("Final price: " + getNumberOfGuest * getPrice);
+        System.out.println("Large event: " + (getNumberOfGuest > cutOffVal));
+
+    }
+
+}
